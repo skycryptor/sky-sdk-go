ec3f74586cc8ee987be0c7ca98d1290d12448c70
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2018-07-09 10:36:27 +0400

    making build scripts and cgo build fixes

--- builder.sh
+++ builder.sh
@@ -2,14 +2,14 @@
 LIBNAME="cryptomagic"
 LIBFILE="lib$LIBNAME.a"
 SOURCE_DIR="`pwd`"
-if [ ! -d "$LIBNAME" ]; then
-  git clone git@gitlab.com:skycryptor/cpp-tmp-crypto.git "$LIBNAME"
-fi
 
+# Cloning and building C++ library
+git clone git@gitlab.com:skycryptor/cpp-tmp-crypto.git "$LIBNAME"
 cd cryptomagic
 rm -rf build
 mkdir -p build && cd build
 
 cmake ..
 make -j4
-cp $LIBFILE $SOURCE_DIR
+cp "$LIBFILE" "$SOURCE_DIR"
+cd "$SOURCE_DIR" && rm -rf "$LIBNAME"
